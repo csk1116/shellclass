c4353b65c956de60bd9b00c7968500cc25ca260a
csk1116 <u1116eric777@gmail.com>
2023-02-08 22:29:35 +0800

    add localuser.

--- localusers/luser-demo01.sh
+++ localusers/luser-demo01.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
 # This script displays various info to the screen.
+# Display #!
+echo '# sharp + ! bang = #! shebang'
 
-# Display Hello
-echo 'Hello'
+# Assign a value to a variable
+WORD='script'
+
+# Display that value using the variable (in convention, all upper case)
+echo "$WORD"
+
+# Combine the variable with hard-cided text.
+echo "This is a shell $WORD"
+echo "This is a shell ${WORD}"
+
+# Append text to the variable
+echo "${WORD}ing is fun." 
+
+ENDING='ed'
+
+echo "This is ${WORD}${ENDING}."
+
+# Reassignment
+ENDING='ing'
+echo "This is ${WORD}${ENDING}."
+
